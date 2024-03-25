99350b1013893c1c4327493c75ded45e07fa8093
Jannah Quetzal <torrhdzj@outlook.com>
2024-03-25 15:41:58 -0600

    carta porte 30 init

---
+++ complemento/cartaporte30/cartporte30_encode.go
@@ -0,0 +1,277 @@
+package cartaporte30
+
+import (
+	"bytes"
+
+	"github.com/veyronifs/cfdi-go/encoder"
+)
+
+var cartaporte30XS = encoder.NSElem{
+	Prefix: "cartaporte30",
+	NS:     "http://www.sat.gob.mx/Cartaporte30",
+}
+
+func (cp *Cartaporte30) SchemaLocation() string {
+	return cartaporte30XS.NS + " http://www.sat.gob.mx/sitio_internet/cfd/CartaPorte/Cartaporte30.xsd"
+}
+
+func (cp *Cartaporte30) XmlNSPrefix() string {
+	return cartaporte30XS.Prefix
+}
+
+func (cp *Cartaporte30) XmlNS() string {
+	return cartaporte30XS.NS
+}
+
+func Marshal(cp *Cartaporte30, moneda string) ([]byte, error) {
+	b := bytes.Buffer{}
+	enc := encoder.NewEncoder(&b)
+	cp.MarshalComplemento(enc, moneda)
+	enc.EndAllFlush()
+	return b.Bytes(), enc.GetError()
+}
+
+func (cp *Cartaporte30) MarshalComplemento(enc *encoder.Encoder, moneda string) {
+	if cp == nil {
+		return
+	}
+	enc.StartElem(cartaporte30XS.Elem("CartaPorte"))
+	defer enc.EndElem("CartaPorte")
+
+	enc.WriteAttrStrZ("Version", cp.Version)
+	enc.WriteAttrStrZ("TranspInternac", cp.TranspInternac)
+	enc.WriteAttrStrZ("EntradaSalidaMerc", cp.EntradaSalidaMerc)
+	enc.WriteAttrStrZ("PaisOrigenDestino", string(cp.PaisOrigenDestino))
+	enc.WriteAttrStrZ("ViaEntradaSalida", cp.ViaEntradaSalida)
+	enc.WriteAttrDecimalZ("TotalDistRec", cp.TotalDistRec, 2)
+
+	encodeUbicaciones(enc, cp.Ubicaciones)
+	encodeMercancias(enc, cp.Mercancias)
+	encodeFiguraTransporte(enc, cp.FiguraTransporte)
+
+}
+
+func encodeUbicaciones(enc *encoder.Encoder, ubicaciones []*Ubicacion) {
+	enc.StartElem(cartaporte30XS.Elem("Ubicaciones"))
+	defer enc.EndElem("Ubicaciones")
+	for _, u := range ubicaciones {
+		encodeUbicacionesUbicacion(enc, u)
+	}
+}
+func encodeUbicacionesUbicacion(enc *encoder.Encoder, u *Ubicacion) {
+	enc.StartElem(cartaporte30XS.Elem("Ubicacion"))
+	defer enc.EndElem("Ubicacion")
+	enc.WriteAttrStrZ("TipoUbicacion", u.TipoUbicacion)
+	enc.WriteAttrStrZ("IDUbicacion", u.IDUbicacion)
+	enc.WriteAttrStrZ("RFCRemitenteDestinatario", u.RFCRemitenteDestinatario)
+	enc.WriteAttrStrZ("NombreRemitenteDestinatario", u.NombreRemitenteDestinatario)
+	enc.WriteAttrStrZ("NumRegIdTrib", u.NumRegIdTrib)
+	enc.WriteAttrStrZ("ResidenciaFiscal", string(u.ResidenciaFiscal))
+	enc.WriteAttrStrZ("NumEstacion", u.NumEstacion)
+	enc.WriteAttrStrZ("NombreEstacion", u.NombreEstacion)
+	enc.WriteAttrStrZ("NavegacionTrafico", u.NavegacionTrafico)
+	enc.WriteAttrStrZ("FechaHoraSalidaLlegada", u.FechaHoraSalidaLlegada.String())
+	enc.WriteAttrStrZ("TipoEstacion", u.TipoEstacion)
+	enc.WriteAttrDecimalZ("DistanciaRecorrida", u.DistanciaRecorrida, 2)
+
+	if u.Domicilio == nil {
+		return
+	}
+	enc.StartElem(cartaporte30XS.Elem("Domicilio"))
+	defer enc.EndElem("Domicilio")
+
+	enc.WriteAttrStrZ("Calle", u.Domicilio.Calle)
+	enc.WriteAttrStrZ("NumeroExterior", u.Domicilio.NumeroExterior)
+	enc.WriteAttrStrZ("NumeroInterior", u.Domicilio.NumeroInterior)
+	enc.WriteAttrStrZ("Colonia", u.Domicilio.Colonia)
+	enc.WriteAttrStrZ("Localidad", u.Domicilio.Localidad)
+	enc.WriteAttrStrZ("Referencia", u.Domicilio.Referencia)
+	enc.WriteAttrStrZ("Municipio", u.Domicilio.Municipio)
+	enc.WriteAttrStrZ("Estado", u.Domicilio.Estado)
+	enc.WriteAttrStrZ("Pais", string(u.Domicilio.Pais))
+	enc.WriteAttrStrZ("CodigoPostal", u.Domicilio.CodigoPostal)
+}
+
+func encodeMercancias(enc *encoder.Encoder, mercancias *Mercancias) {
+	if mercancias == nil {
+		return
+	}
+	enc.StartElem(cartaporte30XS.Elem("Mercancias"))
+	defer enc.EndElem("Mercancias")
+
+	enc.WriteAttrDecimalZ("PesoBrutoTotal", mercancias.PesoBrutoTotal, 3)
+	enc.WriteAttrStrZ("UnidadPeso", mercancias.UnidadPeso)
+	enc.WriteAttrDecimalZ("PesoNetoTotal", mercancias.PesoNetoTotal, 3)
+	enc.WriteAttrInt("NumTotalMercancias", mercancias.NumTotalMercancias)
+	enc.WriteAttrDecimalZ("CargoPorTasacion", mercancias.CargoPorTasacion, 2)
+
+	for _, m := range mercancias.Mercancia {
+		encodeMercancia(enc, m)
+	}
+
+	encodeAutotransporte(enc, mercancias.Autotransporte)
+	encodeTransporteMaritimo(enc, mercancias.TransporteMaritimo)
+	encodeTransporteAereo(enc, mercancias.TransporteAereo)
+	encodeTransporteFerroviario(enc, mercancias.TransporteFerroviario)
+}
+
+func encodeMercancia(enc *encoder.Encoder, m *Mercancia) {
+	enc.StartElem(cartaporte30XS.Elem("Mercancia"))
+	defer enc.EndElem("Mercancia")
+
+	enc.WriteAttrStrZ("BienesTransp", m.BienesTransp)
+	enc.WriteAttrStrZ("ClaveSTCC", m.ClaveSTCC)
+	enc.WriteAttrStrZ("Descripcion", m.Descripcion)
+	enc.WriteAttrDecimalZ("Cantidad", m.Cantidad, 6)
+	enc.WriteAttrStrZ("ClaveUnidad", m.ClaveUnidad)
+	enc.WriteAttrStrZ("Unidad", m.Unidad)
+	enc.WriteAttrStrZ("Dimensiones", m.Dimensiones)
+	enc.WriteAttrStrZ("MaterialPeligroso", m.MaterialPeligroso)
+	enc.WriteAttrStrZ("CveMaterialPeligroso", m.CveMaterialPeligroso)
+	enc.WriteAttrStrZ("Embalaje", m.Embalaje)
+	enc.WriteAttrStrZ("DescripEmbalaje", m.DescripEmbalaje)
+	enc.WriteAttrDecimalZ("PesoEnKg", m.PesoEnKg, 3)
+	enc.WriteAttrDecimalZ("ValorMercancia", m.ValorMercancia, 2)
+	enc.WriteAttrStrZ("Moneda", string(m.Moneda))
+	enc.WriteAttrStrZ("FraccionArancelaria", m.FraccionArancelaria)
+	enc.WriteAttrStrZ("UUIDComercioExt", m.UUIDComercioExt)
+
+	for _, p := range m.Pedimentos {
+		enc.StartElem(cartaporte30XS.Elem("Pedimentos"))
+		enc.WriteAttrStrZ("Pedimento", p.Pedimento)
+		enc.EndElem("Pedimentos")
+	}
+
+	for _, g := range m.GuiasIdentificacion {
+		enc.StartElem(cartaporte30XS.Elem("GuiasIdentificacion"))
+		enc.WriteAttrStrZ("NumeroGuiaIdentificacion", g.NumeroGuiaIdentificacion)
+		enc.WriteAttrStrZ("DescripGuiaIdentificacion", g.DescripGuiaIdentificacion)
+		enc.WriteAttrDecimalZ("PesoGuiaIdentificacion", g.PesoGuiaIdentificacion, 3)
+		enc.EndElem("GuiasIdentificacion")
+	}
+
+	for _, c := range m.CantidadTransporta {
+		enc.StartElem(cartaporte30XS.Elem("CantidadTransporta"))
+		enc.WriteAttrDecimalZ("Cantidad", c.Cantidad, 6)
+		enc.WriteAttrStrZ("IDOrigen", c.IDOrigen)
+		enc.WriteAttrStrZ("IDDestino", c.IDDestino)
+		enc.WriteAttrStrZ("CvesTransporte", c.CvesTransporte)
+		enc.EndElem("CantidadTransporta")
+	}
+
+	if m.DetalleMercancia != nil {
+		enc.StartElem(cartaporte30XS.Elem("DetalleMercancia"))
+		enc.WriteAttrStrZ("UnidadPesoMerc", m.DetalleMercancia.UnidadPesoMerc)
+		enc.WriteAttrDecimalZ("PesoBruto", m.DetalleMercancia.PesoBruto, 3)
+		enc.WriteAttrDecimalZ("PesoNeto", m.DetalleMercancia.PesoNeto, 3)
+		enc.WriteAttrDecimalZ("PesoTara", m.DetalleMercancia.PesoTara, 3)
+		enc.WriteAttrInt("NumPiezas", m.DetalleMercancia.NumPiezas)
+		enc.EndElem("DetalleMercancia")
+	}
+}
+
+func encodeAutotransporte(enc *encoder.Encoder, at *Autotransporte) {
+	if at == nil {
+		return
+	}
+	enc.StartElem(cartaporte30XS.Elem("Autotransporte"))
+	defer enc.EndElem("Autotransporte")
+
+	enc.WriteAttrStrZ("PermSCT", at.PermSCT)
+	enc.WriteAttrStrZ("NumPermisoSCT", at.NumPermisoSCT)
+	if idV := at.IdentificacionVehicular; idV != nil {
+		enc.StartElem(cartaporte30XS.Elem("IdentificacionVehicular"))
+		enc.WriteAttrStrZ("ConfigVehicular", idV.ConfigVehicular)
+		enc.WriteAttrStrZ("PlacaVM", idV.PlacaVM)
+		enc.WriteAttrStrZ("AnioModeloVM", idV.AnioModeloVM)
+		enc.EndElem("IdentificacionVehicular")
+	}
+
+	if seg := at.Seguros; seg != nil {
+		enc.StartElem(cartaporte30XS.Elem("Seguros"))
+		enc.WriteAttrStrZ("AseguraRespCivil", seg.AseguraRespCivil)
+		enc.WriteAttrStrZ("PolizaRespCivil", seg.PolizaRespCivil)
+		enc.WriteAttrStrZ("AseguraMedAmbiente", seg.AseguraMedAmbiente)
+		enc.WriteAttrStrZ("PolizaMedAmbiente", seg.PolizaMedAmbiente)
+		enc.WriteAttrStrZ("AseguraCarga", seg.AseguraCarga)
+		enc.WriteAttrStrZ("PolizaCarga", seg.PolizaCarga)
+		enc.WriteAttrDecimal("PrimaSeguro", seg.PrimaSeguro, 2)
+		enc.EndElem("Seguros")
+	}
+
+	if len(at.Remolques) > 0 {
+		enc.StartElem(cartaporte30XS.Elem("Remolques"))
+		for _, rem := range at.Remolques {
+			enc.StartElem(cartaporte30XS.Elem("Remolque"))
+			enc.WriteAttrStrZ("SubTipoRem", rem.SubTipoRem)
+			enc.WriteAttrStrZ("Placa", rem.Placa)
+			enc.EndElem("Remolque")
+		}
+		enc.EndElem("Remolques")
+	}
+}
+
+func encodeTransporteMaritimo(enc *encoder.Encoder, tm *TransporteMaritimo) {
+	if tm == nil {
+		return
+	}
+	panic("not implemented")
+}
+func encodeTransporteAereo(enc *encoder.Encoder, ta *TransporteAereo) {
+	if ta == nil {
+		return
+	}
+	panic("not implemented")
+}
+func encodeTransporteFerroviario(enc *encoder.Encoder, tf *TransporteFerroviario) {
+	if tf == nil {
+		return
+	}
+	panic("not implemented")
+}
+
+func encodeFiguraTransporte(enc *encoder.Encoder, ft *FiguraTransporte) {
+	if ft == nil {
+		return
+	}
+	enc.StartElem(cartaporte30XS.Elem("FiguraTransporte"))
+	defer enc.EndElem("FiguraTransporte")
+	for _, f := range ft.TiposFigura {
+		encodeFiguraTransporteTiposFigura(enc, f)
+	}
+}
+
+func encodeFiguraTransporteTiposFigura(enc *encoder.Encoder, ft *TiposFigura) {
+	if ft == nil {
+		return
+	}
+	enc.StartElem(cartaporte30XS.Elem("TiposFigura"))
+	defer enc.EndElem("TiposFigura")
+
+	enc.WriteAttrStrZ("TipoFigura", ft.TipoFigura)
+	enc.WriteAttrStrZ("RFCFigura", ft.RFCFigura)
+	enc.WriteAttrStrZ("NumLicencia", ft.NumLicencia)
+	enc.WriteAttrStrZ("NombreFigura", ft.NombreFigura)
+	enc.WriteAttrStrZ("NumRegIdTribFigura", ft.NumRegIdTribFigura)
+	enc.WriteAttrStrZ("ResidenciaFiscalFigura", string(ft.ResidenciaFiscalFigura))
+	for _, part := range ft.PartesTransporte {
+		enc.StartElem(cartaporte30XS.Elem("PartesTransporte"))
+		enc.WriteAttrStrZ("ParteTransporte", part.ParteTransporte)
+		enc.EndElem("PartesTransporte")
+	}
+
+	if dom := ft.Domicilio; dom != nil {
+		enc.StartElem(cartaporte30XS.Elem("Domicilio"))
+		enc.WriteAttrStrZ("Calle", dom.Calle)
+		enc.WriteAttrStrZ("NumeroExterior", dom.NumeroExterior)
+		enc.WriteAttrStrZ("NumeroInterior", dom.NumeroInterior)
+		enc.WriteAttrStrZ("Colonia", dom.Colonia)
+		enc.WriteAttrStrZ("Localidad", dom.Localidad)
+		enc.WriteAttrStrZ("Referencia", dom.Referencia)
+		enc.WriteAttrStrZ("Municipio", dom.Municipio)
+		enc.WriteAttrStrZ("Estado", dom.Estado)
+		enc.WriteAttrStrZ("Pais", string(dom.Pais))
+		enc.WriteAttrStrZ("CodigoPostal", dom.CodigoPostal)
+		enc.EndElem("Domicilio")
+	}
+}
---
+++ complemento/cartaporte30/cartporte30_test.go
@@ -0,0 +1,214 @@
+package cartaporte30
+
+import (
+	"testing"
+
+	"github.com/shopspring/decimal"
+	"github.com/stretchr/testify/assert"
+	"github.com/veyronifs/cfdi-go/types"
+)
+
+func TestUnmarshal(t *testing.T) {
+	var xmlOriginal []byte
+	{
+		xmlOriginal = []byte(`
+			<cartaporte30:CartaPorte Version="2.0" TranspInternac="No" TotalDistRec="1963">
+				<cartaporte30:Ubicaciones>
+					<cartaporte30:Ubicacion TipoUbicacion="Origen" RFCRemitenteDestinatario="IXS7607092R5" FechaHoraSalidaLlegada="2021-12-15T12:24:21">
+						<cartaporte30:Domicilio Calle="Filomeno Mata 922" NumeroExterior="35" Referencia="Filomeno Mata 922, Ejidos de Santa María Aztahuacán, DIF" Estado="DIF" Pais="MEX" CodigoPostal="02900" />
+					</cartaporte30:Ubicacion>
+					<cartaporte30:Ubicacion TipoUbicacion="Destino" RFCRemitenteDestinatario="AMX120216J89" FechaHoraSalidaLlegada="2021-12-15T03:00:00" DistanciaRecorrida="1963">
+						<cartaporte30:Domicilio Calle="TAMAULIPAS 7" NumeroExterior="7" Referencia="TAMAULIPAS 7, CENTRO, SON" Estado="SON" Pais="MEX" CodigoPostal="83001" />
+					</cartaporte30:Ubicacion>
+				</cartaporte30:Ubicaciones>
+				<cartaporte30:Mercancias PesoBrutoTotal="1" UnidadPeso="KGM" NumTotalMercancias="1">
+					<cartaporte30:Mercancia BienesTransp="50425400" Descripcion="CACAHUATE RUNNER 38/42 B R" Cantidad="1" ClaveUnidad="KGM" PesoEnKg="1" />
+					<cartaporte30:Autotransporte PermSCT="TPAF02" NumPermisoSCT="09381581/080002">
+						<cartaporte30:IdentificacionVehicular ConfigVehicular="T3S2" PlacaVM="LE57937" AnioModeloVM="2008" />
+						<cartaporte30:Seguros AseguraRespCivil="Seguros Atlas" PolizaRespCivil="MS1-1-7-38294" AseguraMedAmbiente="Seguros Atlas" PolizaMedAmbiente="MS1-1-7-38294" AseguraCarga="Seguros Atlas" PolizaCarga="MS1-2-2-1353" />
+						<cartaporte30:Remolques>
+							<cartaporte30:Remolque SubTipoRem="CTR004" Placa="6HU3452" />
+						</cartaporte30:Remolques>
+					</cartaporte30:Autotransporte>
+				</cartaporte30:Mercancias>
+				<cartaporte30:FiguraTransporte>
+					<cartaporte30:TiposFigura TipoFigura="01" RFCFigura="VAEE770919BE9" NombreFigura="MANUEL ENRIQUE VELAZQUEZ ESPINOSA" NumLicencia="N03626578" />
+				</cartaporte30:FiguraTransporte>
+			</cartaporte30:CartaPorte>
+		`)
+	}
+
+	cartaPorteUnmarshaled, err := Unmarshal(xmlOriginal)
+	if err != nil {
+		t.Errorf("Error Unmarshal(xmlOriginal): %s", err)
+	}
+
+	xmlMarshaled, err := Marshal(cartaPorteUnmarshaled, "MXN")
+	if err != nil {
+		t.Errorf("Error Marshal(cartaPorteUnmarshal): %s", err)
+	}
+
+	cartaPorteUnmarshaled2, err := Unmarshal(xmlMarshaled)
+	if err != nil {
+		t.Errorf("Error Unmarshal(xmlMarshaled): %s", err)
+	}
+	err = CompareEqual(cartaPorteUnmarshaled, cartaPorteUnmarshaled2)
+	assert.NoError(t, err)
+
+}
+
+func TestMarshal(t *testing.T) {
+	var cartaPorte *Cartaporte30
+	{
+		fechaHoraSalidaLlegada, _ := types.NewFechaH("2021-12-15T03:00:00")
+		cartaPorte = &Cartaporte30{
+			//Mercancias:0xc00010e100
+			Version:           "2.0",
+			TranspInternac:    "No",
+			EntradaSalidaMerc: "Entrada",
+			PaisOrigenDestino: "USA",
+			ViaEntradaSalida:  "01",
+			TotalDistRec:      decimal.New(1963, 0),
+			Ubicaciones: []*Ubicacion{
+				{
+					TipoUbicacion:               "Origen",
+					IDUbicacion:                 "00001",
+					RFCRemitenteDestinatario:    "IXS7607092R5",
+					NombreRemitenteDestinatario: "PEPE",
+					NumRegIdTrib:                "123456789",
+					ResidenciaFiscal:            "MEX",
+					NumEstacion:                 "PM001",
+					NombreEstacion:              "ESTACION PM001",
+					NavegacionTrafico:           "Cabotaje",
+					FechaHoraSalidaLlegada:      fechaHoraSalidaLlegada,
+					TipoEstacion:                "PM",
+					DistanciaRecorrida:          decimal.New(1963, 0),
+					Domicilio: &Domicilio{
+						Calle:          "Calle",
+						NumeroExterior: "123",
+						NumeroInterior: "456",
+						Colonia:        "Colonia",
+						Localidad:      "Localidad",
+						Referencia:     "Referencia",
+						Municipio:      "Municipio",
+						Estado:         "Estado",
+						Pais:           "MEX",
+						CodigoPostal:   "12345",
+					},
+				},
+			},
+			Mercancias: &Mercancias{
+				UnidadPeso:         "KGM",
+				NumTotalMercancias: 1,
+				PesoBrutoTotal:     decimal.New(199, 99),
+				PesoNetoTotal:      decimal.New(199, 99),
+				CargoPorTasacion:   decimal.New(199, 99),
+				Mercancia: []*Mercancia{
+					{
+						BienesTransp:         "10101501",
+						ClaveSTCC:            "10101501",
+						Descripcion:          "Caja de 20 piezas",
+						Cantidad:             decimal.New(10, 0),
+						ClaveUnidad:          "CJ",
+						Unidad:               "Caja",
+						Dimensiones:          "20x20x20",
+						MaterialPeligroso:    "Sí",
+						CveMaterialPeligroso: "M0001",
+						Embalaje:             "1A2",
+						DescripEmbalaje:      "Caja de 20 piezas",
+						PesoEnKg:             decimal.New(199, 99),
+						ValorMercancia:       decimal.New(199, 99),
+						Moneda:               "MXN",
+						FraccionArancelaria:  "10101501",
+						UUIDComercioExt:      "6713E766-DCA2-41AA-B1C0-020CFB60AC95",
+						Pedimentos: []*Pedimentos{
+							{Pedimento: "123456789"},
+						},
+						GuiasIdentificacion: []*GuiasIdentificacion{
+							{
+								NumeroGuiaIdentificacion:  "123456789",
+								DescripGuiaIdentificacion: "Guia de identificacion",
+								PesoGuiaIdentificacion:    decimal.New(199, 99),
+							},
+						},
+						DetalleMercancia: &DetalleMercancia{
+							UnidadPesoMerc: "KGM",
+							PesoBruto:      decimal.New(199, 99),
+							PesoNeto:       decimal.New(199, 99),
+							PesoTara:       decimal.New(199, 99),
+							NumPiezas:      1,
+						},
+						CantidadTransporta: []*CantidadTransporta{
+							{
+								Cantidad:       decimal.New(1, 0),
+								IDOrigen:       "00001",
+								IDDestino:      "00002",
+								CvesTransporte: "1A2",
+							},
+						},
+					},
+				},
+				Autotransporte: &Autotransporte{
+					PermSCT:       "TPAF02",
+					NumPermisoSCT: "09381581/080002",
+					IdentificacionVehicular: &IdentificacionVehicular{
+						ConfigVehicular: "T3S2",
+						PlacaVM:         "LE57937",
+						AnioModeloVM:    "2008",
+					},
+					Seguros: &Seguros{
+						AseguraRespCivil:   "Seguros Atlas",
+						PolizaRespCivil:    "MS1-1-7-38294",
+						AseguraMedAmbiente: "Seguros Atlas",
+						PolizaMedAmbiente:  "MS1-1-7-38294",
+						AseguraCarga:       "Seguros Atlas",
+						PolizaCarga:        "MS1-2-2-1353",
+						PrimaSeguro:        decimal.New(10, 0),
+					},
+					Remolques: []*Remolque{
+						{SubTipoRem: "CTR004", Placa: "6HU3452"},
+						{SubTipoRem: "CTR001", Placa: "6HU3112"},
+					},
+				},
+			},
+			FiguraTransporte: &FiguraTransporte{
+				TiposFigura: []*TiposFigura{
+					{
+						TipoFigura:             "01",
+						RFCFigura:              "VAEE770919BE9",
+						NumLicencia:            "N03626578",
+						NombreFigura:           "MANUEL ENRIQUE VELAZQUEZ ESPINOSA",
+						NumRegIdTribFigura:     "0132456789",
+						ResidenciaFiscalFigura: types.Pais("MEX"),
+						PartesTransporte: []*PartesTransporte{
+							{ParteTransporte: "ABCD"},
+						},
+						Domicilio: &Domicilio{
+							Calle:          "Calle",
+							NumeroExterior: "123",
+							NumeroInterior: "456",
+							Colonia:        "Colonia",
+							Localidad:      "Localidad",
+							Referencia:     "Referencia",
+							Municipio:      "Municipio",
+							Estado:         "Estado",
+							Pais:           "MEX",
+							CodigoPostal:   "12345",
+						},
+					},
+				},
+			},
+		}
+	}
+	xmlMarshaled, err := Marshal(cartaPorte, "MXN")
+	if err != nil {
+		t.Errorf("Error Marshal(cartaPorte): %s", err)
+	}
+
+	cartaPorteUnmarshaled, err := Unmarshal(xmlMarshaled)
+	if err != nil {
+		t.Errorf("Error Unmarshal(xmlMarshaled): %s", err)
+	}
+	err = CompareEqual(cartaPorte, cartaPorteUnmarshaled)
+	assert.NoError(t, err)
+}
